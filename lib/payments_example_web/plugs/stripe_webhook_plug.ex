3d37fd64b09ca8f2a42dbeff7030f4bce50d65d7
Eduardo <eduardodflpereira@gmail.com>
2022-10-11 12:21:12 +0100

    Fix warning

--- lib/payments_example_web/plugs/stripe_webhook_plug.ex
+++ lib/payments_example_web/plugs/stripe_webhook_plug.ex
@@ -9,8 +9,8 @@ defmodule PaymentsExampleWeb.StripeWebhooksPlug do
     signing_secret = Application.get_env(:stripity_stripe, :signing_secret)
     [stripe_signature] = Plug.Conn.get_req_header(conn, "stripe-signature")
 
-    with {:ok, body, _} = Plug.Conn.read_body(conn),
-         {:ok, stripe_event} = Stripe.Webhook.construct_event(body, stripe_signature, signing_secret)
+    with {:ok, body, _} <- Plug.Conn.read_body(conn),
+         {:ok, stripe_event} <- Stripe.Webhook.construct_event(body, stripe_signature, signing_secret)
     do
       Plug.Conn.assign(conn, :stripe_event, stripe_event)
     else
